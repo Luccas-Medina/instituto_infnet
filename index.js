f930657600a432cad93bc7cb5c1b8916001f8a7a
Luccas-Medina <161531488+Luccas-Medina@users.noreply.github.com>
2024-11-09 09:33:33 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+const resultado = document.getElementById('resultado');
+const parlamentares = document.getElementById('parlamentar');
+
+async function buscaPartidos() {
+    await fetch('https://dadosabertos.camara.leg.br/api/v2/partidos')
+        .then(response => response.json())
+        .then(data => {
+            resultado.innerHTML = ''; // Limpa o conteúdo anterior
+            data.dados.forEach(partido => {
+                resultado.innerHTML += `<button onclick='buscaParlamentar(${partido.id})'>Nome: ${partido.nome}</button>`;
+            });
+        })
+        .catch(error => console.error('Erro:', error));
+}
+
+async function buscaParlamentar(idPartido) {
+    await fetch(`https://dadosabertos.camara.leg.br/api/v2/partidos/${idPartido}/membros`)
+        .then(response => response.json())
+        .then(data => {
+            parlamentares.innerHTML = '';
+            data.dados.forEach(parlamentar => {
+                parlamentares.innerHTML += `<p>Nome: ${parlamentar.nome}, Partido: ${parlamentar.siglaPartido}</p>`;
+            });
+        })
+        .catch(error => console.error('Erro:', error));
+}
